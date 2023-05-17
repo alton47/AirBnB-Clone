1ba3d8a267b314da09b91c4f3912935562c41477
Allan <altonallan47@gmail.com>
2023-05-17 10:44:52 +0200

    Add vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,15 @@ import './App.css'
 function App() {
 
   return (
-      <div className='underline mx-11'>
-        Niajee
+      <div className=''>
+        <header>
+          <a href="" className='logo'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+</svg>
+
+          </a>
+        </header>
       </div>
   )
 }
